17fb2ffcf4536b4ee5557073fb6de805e391badd
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-05-01 02:07:31 +0100

    Commit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ const Notfound = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
       <h1 className="text-7xl font-bold mb-2">404</h1>
-      <p className="text-lg text-gray-600">This page could not be found.</p>
+      <p className="text-lg text-gray-700">This page could not be found</p>
     </div>
   )
 }
