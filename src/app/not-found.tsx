31df44434c86cda7e35671859b053caa53997384
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-05-01 01:58:39 +0100

    Added 404 page and the register page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center text-center px-4">
+      <h1 className="text-7xl font-bold mb-2">404</h1>
+      <p className="text-lg text-gray-600">This page could not be found.</p>
+    </div>
+  )
+}
+
+export default Notfound
