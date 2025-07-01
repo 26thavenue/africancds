37fba755a8f98d7a38ab707eadacf8a73eefe0a2
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-07-01 16:20:34 +0100

    Fixed build errors

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -4,36 +4,8 @@ import Link from 'next/link';
 import Navbar from "../components/Navbar"
 import { useRef, useEffect } from 'react'
 import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import newsData from '../lib/news';
 
-
-// lib/news.ts
-export const newsData = [
-  {
-    id: 'summit-security-collab',
-    title: 'African Defense Summit Focuses on Regional Security Collaboration',
-    summary: 'Top military leaders from across Africa gather in Abuja to strengthen joint military operations, intelligence sharing, and peacekeeping efforts.',
-    content: `The African Defense Summit kicked off with over 30 African nations discussing ways to unify regional military strategies. Key topics included counter-terrorism, border control, and cybersecurity infrastructure.`,
-    date: '2025-07-01',
-    imgAddress:"https://images.unsplash.com/photo-1723088672878-045d1dc32cc7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8TmlnZXJpYSUyME1pbGl0YXJ5fGVufDB8fDB8fHww"
-  },
-  {
-    id: 'drone-tech-advancement',
-    title: 'Breakthroughs in African Drone Technology Unveiled at Defense Expo',
-    summary: 'Indigenous drone systems designed for surveillance, rescue missions, and tactical strikes were showcased by defense startups from Kenya, Nigeria, and South Africa.',
-    content: `At the Defense Tech Expo 2025, emerging African tech firms unveiled next-gen UAVs aimed at supporting defense operations and reducing reliance on imported systems.`,
-    date: '2025-06-29',
-    imgAddress:"https://images.unsplash.com/photo-1510181794251-e94190c17f48?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fE5pZ2VyaWElMjBNaWxpdGFyeXxlbnwwfHwwfHx8MA%3D%3D"
-  },
-  {
-    id: 'peacekeeping-initiative',
-    title: 'New African Peacekeeping Initiative Targets Conflict Zones',
-    summary: 'A collaborative initiative backed by the AU aims to deploy 5,000 rapid response troops to stabilize high-risk areas across West and Central Africa.',
-    content: `The AU’s new peacekeeping plan proposes faster deployment, digital command centers, and local community engagement to reduce unrest in volatile zones.`,
-    date: '2025-06-27',
-    imgAddress:"https://images.unsplash.com/photo-1685157782880-4479324e907b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fE5pZ2VyaWElMjBNaWxpdGFyeXxlbnwwfHwwfHx8MA%3D%3D"
-  },
-];
 
 
 export default function NewsPage() {
